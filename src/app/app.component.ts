38b60bbddd5d2f122ad05e0801eb304dae0f5fbb
Shubham <Shubham.maheshan@knorex.com>
2021-04-24 23:39:30 +0530

    Added Feature password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'passwordGen';
+  password: string = ""
+  useLetter: boolean = false;
+  useNumber: boolean = false;
+  useSymbol: boolean = false;
+  lengthvalue: number = 0;
+
+  onButtonClick() {
+
+    this.password = ''
+    let validChars = ''
+    const alpha = 'abcdefghijklmnopqrstuvwxyz';
+    const number = 1234567890;
+    const symbol = "!@#$%^&*()_+";
+    if (this.useLetter) {
+      validChars += alpha;
+    }
+
+    if (this.useNumber) {
+      validChars += number;
+    }
+
+    if (this.useSymbol) {
+      validChars += symbol;
+    }
+    let generatedPassword = ''
+    for (let i = 0; i < this.lengthvalue; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
+
+
+  }
+
+  useLetters() {
+    this.useLetter = !this.useLetter;
+  }
+
+  useNumbers() {
+    this.useNumber = !this.useNumber;
+  }
+
+  useSymbols() {
+    this.useSymbol = !this.useSymbol;
+  }
+
+  uselength(value: string) {
+    this.lengthvalue = parseInt(value);
+
+
+  }
 }
